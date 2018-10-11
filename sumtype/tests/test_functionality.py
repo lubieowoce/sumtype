cc66117752a9e990215381837dec6a4a73466f41
lubieowoce <lolzatu2@gmail.com>
2018-10-11 03:30:42 +0200

    Expanded tests to include the future-compatible sumtype class
    Also refactored some tests

--- sumtype/tests/test_functionality.py
+++ sumtype/tests/test_functionality.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
 
 
 
-import sumtype
+import sumtype as st
 # import sumtype.sumtype_slots as sumtype_slots
 # import sumtype.experimental.sumtype_plain_tuple as sumtype_plain_tuple
 
-uniq = sumtype.slots.uniq
+uniq = st.slots.uniq
 
 def options_repr(options: dict) -> str:
 	if not options:
@@ -31,9 +31,10 @@ def options_repr(options: dict) -> str:
 		')'
 	)
 
+
 def test_make_from_bad_spec():
 	try: 
-		res = sumtype.sumtype.untyped(
+		res = st.sumtype.untyped(
 			'_Underscore Space Name', [
 				('Repeated', ['_a', 'as', 'b c']),
 				('Repeated', ['x', 'x'])
@@ -44,15 +45,15 @@ def test_make_from_bad_spec():
 	print('Bad type spec handled OK')
 
 
-def make_void_classdef():
-	class Void(sumtype.sumtype, allow_zero_constructors=True):
+def make_void_classdef(sumtype):
+	class Void(sumtype, allow_zero_constructors=True):
 		pass
 	print("Void classdef OK")
 	return Void
 
 
-def make_void_call():
-	Void = sumtype.sumtype('Void', [], allow_zero_constructors=True)
+def make_void_call(sumtype):
+	Void = sumtype('Void', [], allow_zero_constructors=True)
 	print("Void call OK")
 	return Void
 
@@ -67,44 +68,59 @@ def test_void(Void):
 	assert Void.__module__   == __name__,      Void.__module__
 
 	assert Void._constructors == ()
-	print('All <Void> tests OK')
 
 
 
-def make_thing_classdef(options=None) -> type:
-	if options is None: options = {}
+def make_thing_classdef_new(sumtype, user_options=None) -> type:
+	if user_options is None: user_options = {}
 
-	class Thing(sumtype.sumtype, **options):
+	class Thing(sumtype, **user_options):
 		def Foo(x: int, y: int): ...
 		def Bar(y: str): ...
-		def Zip(hey: float): ...
+		def Zap(hey: float): ...
 		Hop = ...
 		
-	print('<Thing{}> classdef OK'.format(options_repr(options)))
+	print('old <Thing{}> classdef OK'.format(options_repr(user_options)))
 	return Thing
 
 
+def make_thing_classdef_old(sumtype, user_options=None) -> type:
+	if user_options is None: user_options = {}
+	class Thing(sumtype, **user_options):
+		def Foo(x: int, y: int): ...
+		def Bar(y: str): ...
+		def Zap(hey: float): ...
+		def Hop(): ...
+		
+	print('old <Thing{}> classdef OK'.format(options_repr(user_options)))
+	return Thing
 
-def make_thing_call(options=None) -> type:
-	if options is None: options = {}
 
-	Thing = sumtype.sumtype(
+def make_thing_call(sumtype, user_options=None) -> type:
+	if user_options is None: user_options = {}
+
+	Thing = sumtype(
 		'Thing', [
 			('Foo', [('x', int), ('y', int)]),
 			('Bar', [('y', str)]),
-			('Zip', [('hey', float)]),
+			('Zap', [('hey', float)]),
 			('Hop', []),
 		],
-		**options
+		**user_options
 	)
 
-	print('<Thing{}> call OK'.format(options_repr(options)))
+	print('<Thing{}> call OK'.format(options_repr(user_options)))
 	return Thing
 
 
 
-def test_thing(Thing, options=None):
-	if options is None: options = {}
+def test_thing(Thing, user_options=None):
+	if user_options is None: user_options = {}
+	# inclusion test works on items, not dicts
+	assert user_options.items() <= Thing._options.items(), (
+		repr(Thing._options.items() - user_options.items())
+	)
+	options = Thing._options
 
 	import builtins as b
 
@@ -114,7 +130,7 @@ def test_thing(Thing, options=None):
 	
 	foo = Thing.Foo(3, 5)
 	bar = Thing.Bar("nice")
-	zip = Thing.Zip(15.234)
+	zap = Thing.Zap(15.234)
 	hop = Thing.Hop if options.get('constants') else Thing.Hop()
 
 	# print("Attribute access:")
@@ -129,7 +145,8 @@ def test_thing(Thing, options=None):
 			)
 	)
 
-	for val in ('foo', 'bar', 'zip', 'hop'):
+	eval = b.eval
+	for val in ('foo', 'bar', 'zap', 'hop'):
 		val_ = eval(val)
 		# print("\t{}".format(val_))
 		for field in all_variant_fields:
@@ -171,8 +188,8 @@ def test_thing(Thing, options=None):
 
 
 	# print()
-	# print(foo, bar, zip, hop, sep='\n')
-	values = (foo, bar, zip, hop)
+	# print(foo, bar, zap, hop, sep='\n')
+	values = (foo, bar, zap, hop)
 
 	for x in values:
 		expr = 'x == x'
@@ -197,7 +214,7 @@ def test_thing(Thing, options=None):
 			assert res, '{!r} failed for {!r}'.format(expr, (C, args1, args2))
 
 	# doesn't work if the variants have different type specs
-	# args = (3,); C1 = Thing.Bar; C2 = Thing.Zip
+	# args = (3,); C1 = Thing.Bar; C2 = Thing.Zap
 	# expr = "C1(*args) != C2(*args)"; res = eval(expr)
 	# assert res, '{!r} failed for {!r}'.format(expr, (C1, C2, args))
 
@@ -223,18 +240,25 @@ def test_thing(Thing, options=None):
 		for (fields, args2) in b.zip(Thing._variant_id_fields, variant_args2)
 	]
 	
-	left  = 'x._constructor(**kwargs)'
-	right = 'x._replace(**kwargs)'
-	left_  = eval('lambda x, kwargs: '+left)
-	right_ = eval('lambda x, kwargs: '+right) 
-	for (x_, kwargs_) in b.zip(values, variant_kwargs2):
-		if kwargs_ is not None:
-			vleft  = left_ (x_, kwargs_)
-			vright = right_(x_, kwargs_)
+	# import inspect
+	# def eval(s):
+	# 	print('eval({!r})'.format(s))
+	# 	frame = inspect.currentframe().f_back
+	# 	globs, locs = frame.f_globals, frame.f_locals
+	# 	return b.eval(s, globs, locs)
+
+	left  = 'x._constructor(**kwargs2)'
+	right = 'x._replace(**kwargs2)'
+	for (x, kwargs2) in b.zip(values, variant_kwargs2):
+		if kwargs2 is not None:
+			vleft  = eval(left)
+			vright = eval(right)
 			res = vleft == vright
 			assert res, \
 				'\n{!r}\n  -> {!r}\n and \n{!r}\n  -> {!r}\n failed equality test on {!r}'\
-					.format(left, vleft, right, vright, (x_, kwargs_),)
+					.format(left, vleft, right, vright, (x, kwargs2),)
+
+	# eval = b.eval
 
 	bar2 = bar.replace(y="better")
 	assert bar2.y == "better", repr(bar2)
@@ -264,46 +288,37 @@ def test_thing(Thing, options=None):
 
 	left  = 'x'
 	right = 'x.replace(**x.values_dict())'
-	left_  = eval('lambda x: '+left)
-	right_ = eval('lambda x: '+right) 
 	for x_ in values:
-		vleft  = left_ (x_)
-		vright = right_(x_)
+		vleft  = eval(left)
+		vright = eval(right)
 		res = vleft == vright
 		assert res, '\n{!r}\n  -> {!r}\n and \n{!r}\n  -> {!r}\n failed equality test on {!r}'.format(left, vleft, right, vright, x_,)
 
-
-	left  = 'set( x.as_dict().items() )'
+	left = 'set( x.as_dict().items() )'
 	right = 'set([("variant", x._variant)]) | set([(field, getattr(x, field)) for field in fields])'
-	left_  = eval('lambda x, fields: '+left)
-	right_ = eval('lambda x, fields: '+right) 
-	for (x_, fields_) in b.zip(values, Thing._variant_id_fields):
-		vleft  = left_ (x_, fields_)
-		vright = right_(x_, fields_)
+	for (x, fields) in b.zip(values, Thing._variant_id_fields):
+		vleft  = eval('lambda x, fields: '+ left )(x, fields)
+		vright = eval('lambda x, fields: '+ right)(x, fields)
 		res = vleft == vright
-		assert res, '\n{!r}\n  -> {!r}\n and \n{!r}\n  -> {!r}\n failed equality test on {!r}'.format(left, vleft, right, vright, (x_, fields_),)
+		assert res, '\n{!r}\n  -> {!r}\n and \n{!r}\n  -> {!r}\n failed equality test on {!r}'.format(left, vleft, right, vright, (x, fields),)
 
 
 	left  = 'x.as_tuple()'
 	right = '(x.variant,) + tuple(getattr(x, field) for field in fields)'
-	left_  = eval('lambda x, fields: '+left)
-	right_ = eval('lambda x, fields: '+right) 
-	for (x_, fields_) in b.zip(values, Thing._variant_id_fields):
-		vleft  = left_ (x_, fields_)
-		vright = right_(x_, fields_)
+	for (x, fields) in b.zip(values, Thing._variant_id_fields):
+		vleft  = eval('lambda x, fields: '+left)(x, fields)
+		vright = eval('lambda x, fields: '+right)(x, fields)
 		res = vleft == vright
-		assert res, '\n{!r}\n  -> {!r}\n and \n{!r}\n  -> {!r}\n failed equality test on {!r}'.format(left, vleft, right, vright, (x_, fields_),)
+		assert res, '\n{!r}\n  -> {!r}\n and \n{!r}\n  -> {!r}\n failed equality test on {!r}'.format(left, vleft, right, vright, (x, fields),)
 
 
 	left  = 'set( x.as_dict().items() )'
 	right = 'set( zip(("variant",)+fields, x.as_tuple()) )'
-	left_  = eval('lambda x, fields: '+left)
-	right_ = eval('lambda x, fields: '+right) 
-	for (x_, fields_) in b.zip(values, Thing._variant_id_fields):
-		vleft  = left_ (x_, fields_)
-		vright = right_(x_, fields_)
+	for (x, fields) in b.zip(values, Thing._variant_id_fields):
+		vleft  = eval(left)
+		vright = eval(right)
 		res = vleft == vright
-		assert res, '\n{!r}\n  -> {!r}\n and \n{!r}\n  -> {!r}\n failed equality test on {!r}'.format(left, vleft, right, vright, (x_, fields_),)
+		assert res, '\n{!r}\n  -> {!r}\n and \n{!r}\n  -> {!r}\n failed equality test on {!r}'.format(left, vleft, right, vright, (x, fields),)
 
 
 	expr = 'x.values() == x.as_tuple()[1:] == x.__getstate__()[1:]'
@@ -311,38 +326,41 @@ def test_thing(Thing, options=None):
 		res = eval(expr)
 		assert res, '{!r} failed for {!r}'.format(expr, x)
 
-	expr = 'new_x == x'
+	body = [
+		'x_state = x.__getstate__()',
+		'new_x = Thing.__new__(Thing)',
+		'new_x.__setstate__(x_state)',
+		'new_x == x'
+	]
 	for x in values:
-		x_state = x.__getstate__()
-		new_x = Thing.__new__(Thing)
-		new_x.__setstate__(x_state)
-		res = eval(expr)
-		assert res, '{!r} failed for {!r}'.format(expr, x)
+		body[-1] = 'res = '+body[-1]
+		exec('\n'.join(body))
+		assert res, '{!r} failed for {!r}'.format(body, x)
 
 
 	try: res = bar.replace(blah_blah_blah=5)
-	except Exception as e: res = e
+	except AttributeError as e: res = e
 	# print("bad replace 1:", bar, repr(res))
-	assert isinstance(res, Exception), repr(res)
+	assert isinstance(res, AttributeError), repr(res)
 
 	try: res = bar.replace(_0=5)
-	except Exception as e: res = e
+	except TypeError as e: res = e
 	# print("bad replace 2:", bar, repr(res))
-	assert isinstance(res, Exception), repr(res)
+	assert isinstance(res, TypeError), repr(res)
 
 	try: res = bar.replace(_variant_id=5)
-	except Exception as e: res = e
+	except TypeError as e: res = e
 	# print("bad replace 2:", bar, repr(res))
-	assert isinstance(res, Exception), repr(res)
+	assert isinstance(res, TypeError), repr(res)
 
 	foo_ = Thing.Foo(3, 5)
 	Thing._variant_id.__set__(foo_, 15)
 	try:
 		res = repr(foo_)
-	except Exception as e:
+	except RuntimeError as e:
 		res = e
 	# print("repr of bad variant:", repr(res))
-	assert isinstance(res, Exception), repr(res)
+	assert isinstance(res, RuntimeError), repr(res)
 
 	foo_ = Thing.Foo(3, 5)
 	Thing._unsafe_set_Foo_x(foo_, 10)
@@ -355,22 +373,52 @@ def test_thing(Thing, options=None):
 
 	# foo._Foo_y = 10
 	# print(foo)
-	print('All <Thing{}> tests OK'.format(options_repr(options)))
 
 
 if __name__ == '__main__':
 	print('Running tests')
 	print()
-	for options in (dict(), dict(constants=True)):
-		for Thing in (make_thing_classdef(options), make_thing_call(options)):
+	for user_options in (dict(), dict(constants=True), dict(constants=False)):
+		for sumtype in (st.sumtype, st.future.sumtype):
+			for make_thing in (make_thing_classdef_old, make_thing_classdef_new, make_thing_call):
+				Thing = make_thing(sumtype, user_options)
+				err_msg = '{} ~> <Thing{}>'.format(
+					sumtype.__module__+'.'+sumtype.__name__,
+					options_repr(user_options)
+				)
+				try:
+					test_thing(Thing, user_options)
+				except Exception as e:
+					e2 = e.__class__(err_msg)
+					raise e2 from e
+				else:
+					print(
+						'All {} ~> <Thing{}> tests OK'.format(
+							sumtype.__module__+'.'+sumtype.__name__,
+							options_repr(user_options)
+						)
+					)
+					# print(Thing._options )
+			print()
+
+	for sumtype in (st.sumtype, st.future.sumtype):
+		for Void in (make_void_classdef(sumtype), make_void_call(sumtype)):
 			try:
-				test_thing(Thing, options)
-			except AssertionError as e:
-				e2 = AssertionError('<Thing{}>'.format(options_repr(options)))
+				test_void(Void)
+			except Exception as e:
+				e2 = e.__class__(
+					'{} ~> <Void>'.format(
+						sumtype.__module__+'.'+sumtype.__name__,
+					)
+				)
 				raise e2 from e
-	print()
-	for Void in (make_void_classdef(), make_void_call()):
-		test_void(Void)
+			else:
+				print(
+					'All {} ~> <Void> tests OK'.format(
+						sumtype.__module__+'.'+sumtype.__name__,
+					)
+				)
+
 	print()
 
 	test_make_from_bad_spec()
