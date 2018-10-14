3583249ba5aba878121730a4324c84540a57553f
lubieowoce <lolzatu2@gmail.com>
2018-10-14 19:21:06 +0200

    Bump version (beta release)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
 	name     = 'sumtype',
-	version  = '0.10.0.post4',
+	version  = '0.11.b0',
 	packages = find_packages(exclude=['tests']),
 	
 	python_requires = '>=3',
